b48c082880bedfdb44d3467e11ef5d673fa5b335
anujshany <anujshany@gmail.com>
2021-10-02 09:24:52 +0530

    Updates

---
+++ precache-manifest.0acfc04d9da2f11fa5d603a7f51bdd03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43210171f4f6a4f531e3e9af7aa40447",
+    "url": "/Birthday-Reminder-UI-UX/index.html"
+  },
+  {
+    "revision": "4c83ca6c1c7b48801b76",
+    "url": "/Birthday-Reminder-UI-UX/static/css/main.1645cd71.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Birthday-Reminder-UI-UX/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday-Reminder-UI-UX/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c83ca6c1c7b48801b76",
+    "url": "/Birthday-Reminder-UI-UX/static/js/main.87781021.chunk.js"
+  },
+  {
+    "revision": "7a9d5b6321e314e8ad7e",
+    "url": "/Birthday-Reminder-UI-UX/static/js/runtime-main.e28a3988.js"
+  }
+]);
